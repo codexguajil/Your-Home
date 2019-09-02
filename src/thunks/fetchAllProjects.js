cc298898a0bd33af700a6ae2acc63adf989062a7
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-09-01 18:15:27 -0600

    Write deleteMaterial thunk

--- src/thunks/fetchAllProjects.js
+++ src/thunks/fetchAllProjects.js
@@ -1,4 +1,4 @@
-import { isLoading, hasErrored, setProjects, deleteAProject, deleteARoom } from '../actions';
+import { isLoading, hasErrored, setProjects, deleteAProject, deleteARoom, deleteAMaterial } from '../actions';
 
 export const fetchAllProjects = () => {
   return async (dispatch) => {
@@ -69,6 +69,31 @@ export const deleteRoom = (id, projectId) => {
       }
       const data = await response.json()
       dispatch(deleteARoom(id, projectId))
+      dispatch(isLoading(false))
+    } catch(error) {
+      dispatch(hasErrored(error.message))
+    }
+  }
+}
+
+export const deleteMaterial = (materialType, id) => {
+  return async (dispatch) => {
+    const url = "https://hometrackr.herokuapp.com//api/v1/graphql"
+    try {
+      dispatch(isLoading(true));
+      const response = await fetch(url, {
+        body: JSON.stringify({"query":`mutation{deleteRoomMaterial(id: "${id}" )}`
+      }),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      if(!response.ok) {
+        throw Error(response.statusText)
+      }
+      const data = await response.json()
+      dispatch(deleteAMaterial(materialType, id))
       dispatch(isLoading(false))
     } catch(error) {
       dispatch(hasErrored(error.message))
